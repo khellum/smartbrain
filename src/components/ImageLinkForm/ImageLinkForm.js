33e5de2ddf934980ae0bad6c0085cda857ea3f86
Kedir <khellum.dev@gmail.com>
2022-01-09 19:20:45 +0300

    more stylin

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,9 +7,9 @@ const ImageLinkForm = () => {
             <p>{'This smart brain will detect faces in a picture, try it on'}</p>
 
             <div className='center'>
-                <div className='center form pa4 br3 shadow-5'>
-                    <input className="f4 pa2 w-70 center"  type="text" placeholder="Insert an image url"/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-purple"> Detect! </button>
+                <div className='center form pa4 br4 shadow-5'>
+                    <input className="f4 br3 shadow-4 pa2 w-70 center"  type="text" placeholder="Insert an image url"/>
+                    <button className="w-30 grow br3 shadow-5 f4 link ph3 pv2 dib white bg-purple"> Detect! </button>
                 </div>
             </div>
         </div>
